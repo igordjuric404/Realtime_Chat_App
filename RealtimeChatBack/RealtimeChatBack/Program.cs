ad46702cc5c6e7ef2b22d32c71594b6afa1795b3
Igor Djuric <igordjuric404@gmail.com>
2024-05-26 21:35:15 +0200

    initialize project

---
+++ RealtimeChatBack/RealtimeChatBack/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace RealtimeChatBack
+{
+    public class Program
+    {
+        
+    }
+}
---
+++ RealtimeChatBack/RealtimeChatBack/Startup.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic; // Add this directive
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.OpenApi.Models;
+using ChatApplicationYT;
+
+namespace RealtimeChatBack
+{
+    public class Startup
+    {
+        
+    }
+}
